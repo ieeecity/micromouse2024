c8b215fdabb81de2930411bd4eba2da1fcd781b7
zcityuni <126744949+zcityuni@users.noreply.github.com>
2024-04-02 18:03:34 +0100

    Update maze.h

--- maze.h
+++ maze.h
@@ -1,3 +1,6 @@
+
+#include <LinkedList.h>
+
 struct Cell {
   bool walls[4]; // Array to represent walls: 0 -> right, 1 -> left, 2 -> up, 3 -> down
   int x; // X coordinate of the cell
@@ -8,89 +11,89 @@ Cell maze[8][8] = {
   {
     // R     L      U     D
     {{true, true, false, true}, 0, 0, 0}, 
-    {{false, false, false, true}, 1, 0, 0}, 
-    {{false, false, false, true}, 2, 0, 0}, 
-    {{false, false, false, true}, 3, 0, 0}, 
-    {{false, false, false, true}, 4, 0, 0}, 
-    {{false, false, false, true}, 5, 0, 0}, 
-    {{false, false, false, true}, 6, 0, 0}, 
+    {{false, true, true, true}, 1, 0, 0}, 
+    {{true, false, false, true}, 2, 0, 0}, 
+    {{false, true, false, true}, 3, 0, 0}, 
+    {{true, false, true, true}, 4, 0, 0}, 
+    {{false, true, false, true}, 5, 0, 0}, 
+    {{false, false, true, true}, 6, 0, 0}, 
     {{true, false, false, true}, 7, 0, 0}
   },
 
   {
-    {{false, true, false, false}, 0, 1, 0}, 
-    {{false, false, false, false}, 1, 1, 0}, 
-    {{false, false, false, false}, 2, 1, 0}, 
-    {{false, false, false, false}, 3, 1, 0}, 
-    {{false, false, false, false}, 4, 1, 0}, 
-    {{false, false, false, false}, 5, 1, 0}, 
-    {{false, false, false, false}, 6, 1, 0}, 
-    {{true, false, false, false}, 7, 1, 0}
+    {{true, true, false, false}, 0, 1, 0}, 
+    {{true, true, false, true}, 1, 1, 0}, 
+    {{true, true, false, false}, 2, 1, 0}, 
+    {{false, true, true, false}, 3, 1, 0}, 
+    {{false, false, true, true}, 4, 1, 0}, 
+    {{true, false, true, false}, 5, 1, 0}, 
+    {{true, true, false, true}, 6, 1, 0}, 
+    {{true, true, false, false}, 7, 1, 0}
   },
 
   {
     {{false, true, false, false}, 0, 2, 0}, 
-    {{false, false, false, false}, 1, 2, 0}, 
-    {{false, false, false, false}, 2, 2, 0}, 
-    {{false, false, false, false}, 3, 2, 0}, 
-    {{false, false, false, false}, 4, 2, 0}, 
-    {{false, false, false, false}, 5, 2, 0}, 
-    {{false, false, false, false}, 6, 2, 0}, 
+    {{true, false, true, false}, 1, 2, 0}, 
+    {{false, true, true, false}, 2, 2, 0}, 
+    {{false, false, false, true}, 3, 2, 0}, 
+    {{false, false, true, true}, 4, 2, 0}, 
+    {{false, false, true, true}, 5, 2, 0}, 
+    {{false, false, true, false}, 6, 2, 0}, 
     {{true, false, false, false}, 7, 2, 0}
   },
 
   {
     {{false, true, false, false}, 0, 3, 0}, 
-    {{false, false, false, false}, 1, 3, 0}, 
-    {{false, false, false, false}, 2, 3, 0}, 
-    {{false, false, false, false}, 3, 3, 0}, 
-    {{false, false, false, false}, 4, 3, 0}, 
-    {{false, false, false, false}, 5, 3, 0}, 
-    {{false, false, false, false}, 6, 3, 0}, 
+    {{false, false, true, false}, 1, 3, 0}, 
+    {{false, false, false, true}, 2, 3, 0}, 
+    {{true, false, false, false}, 3, 3, 0}, 
+    {{false, true, true, true}, 4, 3, 0}, 
+    {{false, false, true, true}, 5, 3, 0}, 
+    {{false, false, true, true}, 6, 3, 0}, 
     {{true, false, false, false}, 7, 3, 0}
   },
 
   {
-    {{false, true, false, false}, 0, 4, 0}, 
-    {{false, false, false, false}, 1, 4, 0}, 
-    {{false, false, false, false}, 2, 4, 0}, 
-    {{false, false, false, false}, 3, 4, 0}, 
-    {{false, false, false, false}, 4, 4, 0}, 
-    {{false, false, false, false}, 5, 4, 0}, 
-    {{false, false, false, false}, 6, 4, 0}, 
+    {{true, true, false, false}, 0, 4, 0}, 
+    {{false, true, true, true}, 1, 4, 0}, 
+    {{true, false, true, false}, 2, 4, 0}, 
+    {{true, true, false, false}, 3, 4, 0}, 
+    {{true, true, false, true}, 4, 4, 0}, 
+    {{false, true, false, true}, 5, 4, 0}, 
+    {{false, false, true, true}, 6, 4, 0}, 
     {{true, false, false, false}, 7, 4, 0}
   },
 
   {
     {{false, true, false, false}, 0, 5, 0}, 
-    {{false, false, false, false}, 1, 5, 0}, 
-    {{false, false, false, false}, 2, 5, 0}, 
-    {{false, false, false, false}, 3, 5, 0}, 
-    {{false, false, false, false}, 4, 5, 0}, 
-    {{false, false, false, false}, 5, 5, 0}, 
-    {{false, false, false, false}, 6, 5, 0}, 
-    {{true, false, false, false}, 7, 5, 0}
+    {{false, false, false, true}, 1, 5, 0}, 
+    {{true, false, false, true}, 2, 5, 0}, 
+    {{true, true, false, false}, 3, 5, 0}, 
+    {{true, true, false, false}, 4, 5, 0}, 
+    {{false, true, false, false}, 5, 5, 0}, 
+    {{true, false, false, true}, 6, 5, 0}, 
+    {{true, true, false, false}, 7, 5, 0}
   },
 
   {
-    {{false, true, false, false}, 0, 6, 0}, 
-    {{false, false, false, false}, 1, 6, 0}, 
-    {{false, false, false, false}, 2, 6, 0}, 
-    {{false, false, false, false}, 3, 6, 0}, 
-    {{false, false, false, false}, 4, 6, 0}, 
-    {{false, false, false, false}, 5, 6, 0}, 
-    {{false, false, false, false}, 6, 6, 0}, 
-    {{true, false, false, false}, 7, 6, 0}
+    {{true, true, false, false}, 0, 6, 0}, 
+    {{true, true, false, false}, 1, 6, 0}, 
+    {{true, true, false, false}, 2, 6, 0}, 
+    {{false, true, true, false}, 3, 6, 0}, 
+    {{true, false, false, false}, 4, 6, 0}, 
+    {{false, true, true, false}, 5, 6, 0}, 
+    {{true, false, true, false}, 6, 6, 0}, 
+    {{true, true, false, false}, 7, 6, 0}
   },
 
   {
     {{false, true, true, false}, 0, 7, 0}, 
-    {{false, false, true, false}, 1, 7, 0}, 
-    {{false, false, true, false}, 2, 7, 0}, 
-    {{false, false, true, false}, 3, 7, 0}, 
-    {{false, false, true, false}, 4, 7, 0}, 
-    {{false, false, true, false}, 5, 7, 0}, 
-    {{false, false, true, false}, 6, 7, 0}, 
+    {{true, false, true, false}, 1, 7, 0}, 
+    {{false, true, true, false}, 2, 7, 0}, 
+    {{true, false, true, true}, 3, 7, 0}, 
+    {{false, true, true, false}, 4, 7, 0}, 
+    {{false, false, true, true}, 5, 7, 0}, 
+    {{false, false, true, true}, 6, 7, 0}, 
     {{true, false, true, false}, 7, 7, 0}
   }
 };
